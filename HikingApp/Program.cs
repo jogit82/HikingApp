88763f339476c473ca024961eb92e80f03ff4a5a
jogit82 <jo.c.keller@gmail.com>
2017-09-25 17:47:23 -0700

    initial commit

---
+++ HikingApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HikingApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
